6b4c9527b50ed7b526de2d126b8af2da933ed402
shuoryang <shuoryang@gmail.com>
2015-12-16 14:14:29 -0500

    Update process.php

--- process.php
+++ process.php
@@ -1,11 +1,31 @@
+<html>
+<body>
 <?php
-$target_dir = "uploads/";
-$compressrate = $_POST["compressrate"];
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$URL = $_POST["URL"];
+$type = $_POST["type"];
+
 // Check if image file is a actual image or fake image
-echo ("aaaaaaa");
-echo shell_exec("python /usr/local/bin/youtube-dl -f 18 https://www.youtube.com/watch?v=6WP43WfbXTU --prefer-ffmpeg -o '/home/ubuntu/%(title)s.(ext).s'");
+echo ("<h2 align='center'> Your download will start shortly<h2>");
+shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s.%(ext)s'");
+$fileName = shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s.%(ext)s' --get-filename");
+//header("Location: input.html"); /* Redirect browser */
 
 ?>
+
+<form action='download.jsp' method='post' name='frm'>
+<?php
+foreach ($_POST as $a => $b) {
+echo "<input type='hidden' name='".htmlentities($a)."' value='".htmlentities($b)."'>";
+}
+echo "<input type='hidden' name='fileName' value='$fileName'>";
+?>
+<div align="center">
+<noscript><input type="submit" value="Click here if you are not downloading."/></noscript>
+</div>
+</form>
+<script language="JavaScript">
+document.frm.submit();
+</script>
+</body>
+</html>
+
