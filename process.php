d4dca3dcc232ac03d63f912e5944bd0b2492c844
shuoryang <shuoryang@gmail.com>
2015-12-16 17:41:29 -0500

    Update process.php

--- process.php
+++ process.php
@@ -1,12 +1,14 @@
 <html>
+<head>
+<title>Downloading</title>
+</head>
 <body>
 <?php
 $URL = $_POST["URL"];
 $type = $_POST["type"];
 
 // Check if image file is a actual image or fake image
-echo ("<title>Downloading</title>");
-echo ("<h2 align='center'> Enjoy your Video!<h2>");
+echo ("");
 shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s-%(resolution)s.%(ext)s'");
 $fileName = shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s-%(resolution)s.%(ext)s' --get-filename");
 //header("Location: input.html"); /* Redirect browser */
@@ -20,10 +22,9 @@ echo "<input type='hidden' name='".htmlentities($a)."' value='".htmlentities($b)
 }
 echo "<input type='hidden' name='fileName' value='$fileName'>";
 ?>
-<div align="center">
+<div align="center" style="position:fixed; top:20%; left:50%; margin-left:-8em">
+<h2 align='center'> Enjoy your Video!<h2>
 <noscript><input type="submit" value="Click here if you are not downloading."/></noscript>
-</div>
-<div align="center">
 <input type="button" value="Start a new download" onclick="location.href='input.html'" />
 </div>
 </form>
