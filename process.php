d2dbb2346d5e91ba188b949d13d867114aed504d
shuoryang <shuoryang@gmail.com>
2015-12-16 17:11:38 -0500

    Update process.php

--- process.php
+++ process.php
@@ -6,8 +6,8 @@ $type = $_POST["type"];
 
 // Check if image file is a actual image or fake image
 echo ("<h2 align='center'> Your download will start shortly<h2>");
-shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s.%(ext)s'");
-$fileName = shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s.%(ext)s' --get-filename");
+shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s-%(resolution)s.%(ext)s'");
+$fileName = shell_exec("python /usr/local/bin/youtube-dl $URL -f $type --restrict-filenames --prefer-ffmpeg -o 'data/%(title)s-%(resolution)s.%(ext)s' --get-filename");
 //header("Location: input.html"); /* Redirect browser */
 
 ?>
@@ -21,6 +21,9 @@ echo "<input type='hidden' name='fileName' value='$fileName'>";
 ?>
 <div align="center">
 <noscript><input type="submit" value="Click here if you are not downloading."/></noscript>
+</div>
+<div align="center">
+<input type="button" value="Start a new download" onclick="location.href='input.html'" />
 </div>
 </form>
 <script language="JavaScript">
